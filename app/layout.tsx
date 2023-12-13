5aab83f71827e692378ea56dba7f1258342f2718
Oleg Berlo <oleg.b@oxtech.org>
2023-12-13 19:52:03 +0200

    updates

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import { StarsCanvas } from "@/components/main/StarBackground";
 import Navbar from "@/components/main/Navbar";
+import { Footer } from "@/components/main/Footer";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -18,6 +19,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
         <StarsCanvas />
         <Navbar />
         {children}
+        <Footer />
       </body>
     </html>
   );
